0422845ea90fdec7c0de88e31173fe5003e5e39b
Kent Liao <2964556627@qq.com>
2023-03-04 12:03:54 +0800

    抽取js
    Signed-off-by: Kent Liao <2964556627@qq.com>

---
+++ assets/jasmine/jasmine.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    // 粘滞
+    new StickySidebar('#left-sidebar-sticky', {
+        topSpacing: 70,
+        resizeSensor: true
+    })
+    new StickySidebar('#right-sidebar-sticky', {})
+    new StickySidebar('#right-sidebar-sticky', {})
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 150) {
+            $('.jasmine-navbar').addClass('jasmine-navbar-sticky')
+        } else {
+            $('.jasmine-navbar').removeClass('jasmine-navbar-sticky')
+        }
+    })
+    // 提示
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    // 明亮/暗黑模式
+    const darkmode = new darken({
+        class: "darkmode-active",
+        container: "body",
+        toggle: "#darkmode-button",
+        timestamps: {
+            dark: "20:00",
+            light: "6:00",
+        }
+    })
+    // 返回顶部
+    $('#backToTop').on('click', function () {
+        $("html, body").animate({scrollTop: 0}, 100);
+    })
+})
--- footer.php
+++ footer.php
@@ -26,39 +26,9 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 <script src="https://cdn.staticfile.org/bootstrap/5.2.3/js/bootstrap.bundle.min.js"
         type="application/javascript"></script>
 <script src="<?php $this->options->themeUrl('assets/darken/darken.umd.js'); ?>"></script>
+<script src="<?php $this->options->themeUrl('assets/jasmine/jasmine.js'); ?>"></script>
 <script>
-    $(document).ready(function () {
-        new StickySidebar('#left-sidebar-sticky', {
-            topSpacing: 70,
-            resizeSensor: true
-        })
-        new StickySidebar('#right-sidebar-sticky', {})
-        new StickySidebar('#right-sidebar-sticky', {})
-        $(window).scroll(function () {
-            if ($(document).scrollTop() > 150) {
-                $('.jasmine-navbar').addClass('jasmine-navbar-sticky')
-            } else {
-                $('.jasmine-navbar').removeClass('jasmine-navbar-sticky')
-            }
-        })
-        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-        const darkmode = new darken({
-            class: "darkmode-active",
-            container: "body",
-            toggle: "#darkmode-button",
-            timestamps: {
-                dark: "20:00",
-                light: "6:00",
-            }
-        })
-
-        $('#backToTop').on('click', function () {
-            $("html, body").animate({scrollTop: 0}, 100);
-        })
-
-    })
 </script>
 </body>
 </html>
