25a1974aab43fa65787bc984cc3a9b5919c65cf5
Kent Liao <2964556627@qq.com>
2023-03-07 08:48:03 +0800

    feat: add favorites icon
    Signed-off-by: Kent Liao <2964556627@qq.com>

--- header.php
+++ header.php
@@ -32,6 +32,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
             'tag' => _t('标签 %s 下的文章'),
             'author' => _t('%s 发布的文章')
         ], '', ' - '); ?><?php $this->options->title(); ?></title>
+    <link rel="icon" type="image/ico" href="<?php $this->options->siteUrl();?>favicon.ico">
     <link href="https://cdn.staticfile.org/bootstrap/5.2.3/css/bootstrap.min.css" type="text/css" rel="stylesheet"/>
     <link href="https://cdn.staticfile.org/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css" type="text/css" rel="stylesheet"/>
     <link href="<?php $this->options->themeUrl('assets/jasmine/jasmine.css'); ?>" type="text/css" rel="stylesheet">
