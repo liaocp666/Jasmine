8f4c51086ee52415358761476ddbbbabef18e8ea
Kent Liao <2964556627@qq.com>
2023-03-07 09:05:28 +0800

    docs: tp theme details
    Signed-off-by: Kent Liao <2964556627@qq.com>

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 /**
- * 简洁、美观、响应式的 Typecho 主题。
+ * 专为博客类网站开发，简洁、美观、响应式的 Typecho 主题。<br/>
  * Github：<a href="https://github.com/liaocp666/Jasmine" target="_blank" title="Jasmine Github Repo">https://github.com/liaocp666/Jasmine</a>
+ * Demo：<a href="https://www.liaocp.cn/" target="_blank" title="Jasmine Demo Website">https://www.liaocp.cn/</a>
  *
  * @package Jasmine
  * @author Kent Liao
