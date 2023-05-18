414852016ef269cdcda0eaca371fb43a51519034
Kent Liao <2964556627@qq.com>
2023-05-18 21:45:38 +0800

    置顶文章去除默认缩略图
    Signed-off-by: Kent Liao <2964556627@qq.com>

--- component/post-top.php
+++ component/post-top.php
@@ -1,5 +1,5 @@
 <?php if (!defined("__TYPECHO_ROOT_DIR__")) {
-  exit();
+    exit();
 } ?>
 
 <?php
@@ -11,18 +11,17 @@ if (null != $cids): ?>
         <?php foreach ($cids as $cid): ?>
             <?php $this->widget("Widget_Archive@jasmine" . $cid, "pageSize=1&type=post", "cid=" . $cid)->to($item); ?>
             <div class="flex w-1/2 flex-row">
-                <a href="<?php $item->permalink(); ?>" title="<?php $item->title(); ?>" class="w-[130px]"
-                   title="<?php $item->title(); ?>">
-                    <img
-                        src="<?php echo getThumbnail(
-                          $item->cid,
-                          "data:image/gif;base64,R0lGODdhAQABAPAAAMPDwwAAACwAAAAAAQABAAACAkQBADs="
-                        ); ?>"
-                        alt="" width="130"
-                        height="90"
-                        alt="<?php $item->title(); ?>"
-                        class="h-[90px] w-[130px] rounded object-cover hover:shadow-lg"/>
-                </a>
+                <?php if ($thumbnail = getThumbnail($item->cid, "")): ?>
+                    <a href="<?php $item->permalink(); ?>" title="<?php $item->title(); ?>" class="w-[130px]"
+                       title="<?php $item->title(); ?>">
+                        <img
+                            src="<?php echo $thumbnail; ?>"
+                            alt="" width="130"
+                            height="90"
+                            alt="<?php $item->title(); ?>"
+                            class="h-[90px] w-[130px] rounded object-cover"/>
+                    </a>
+                <?php endif; ?>
                 <div class="mx-3 flex flex-1 flex-col justify-between overflow-hidden">
                     <h2 class="line-clamp-1 text-lg">
                         <a href="<?php $item->permalink(); ?>"
@@ -38,4 +37,4 @@ if (null != $cids): ?>
         <div class="w-1/2"></div>
     </div>
 <?php endif;
- ?>
+?>
