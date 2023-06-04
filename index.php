4d55f965b197a9fc9054bd35058ab2d57fcf7cff
Kent Liao <2964556627@qq.com>
2023-06-04 23:40:52 +0800

    增加主题版本

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php if (!defined("__TYPECHO_ROOT_DIR__")) {
+<?php
+/**
+ * 专为博客类网站开发，简洁、美观、响应式的 Typecho 主题。<br/>
+ * Github：<a href="https://github.com/liaocp666/Jasmine" target="_blank" title="Jasmine Github Repo">https://github.com/liaocp666/Jasmine</a>
+ * Demo：<a href="https://www.liaocp.cn/" target="_blank" title="Jasmine Demo Website">https://www.liaocp.cn/</a>
+ *
+ * @package Jasmine
+ * @author Kent Liao
+ * @version 2.0.0
+ * @link https://www.liaocp.cn/
+ */
+if (!defined("__TYPECHO_ROOT_DIR__")) {
   exit();
 } ?>
 <!DOCTYPE html>
