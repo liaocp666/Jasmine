6cb70aebefb4b319f419d78800cc1a50abc64914
Kent Liao <2964556627@qq.com>
2023-06-05 15:23:56 +0800

    tailwindcss 生成配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
-  content: ["*.php", "component/*.php", "./src/main.ts"],
+  content: ["*.php", "component/*.php", "core/*.php", "./src/main.ts"],
   theme: {
     extend: {},
   },
