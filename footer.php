69cd03a84ba72c66159e3104e88bd55cd8a134c1
Kent Liao <2964556627@qq.com>
2023-06-05 20:12:53 +0800

    页脚动态版权

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
     <div class="flex flex-col items-center">
         <div class="flex flex-row gap-x-1 items-center">
             <iconify-icon icon="tabler:copyright" class="text-gray-800 dark:dark:text-gray-400""></iconify-icon>
-            <span><?php echo getCopyrightDate(); ?> <?php $this->options->title() ?>. All Rights Reserved.</span>
+            <span><?php echo getCopyrightDate(); ?> <?php $this->options->title(); ?>. All Rights Reserved.</span>
         </div>
         <span>Theme <a href="https://github.com/liaocp666/Jasmine" title="Jasmine" target="_blank">Jasmine</a> by <a href="https://www.liaocp.cn/" title="Kent Liao" target="_blank">Kent Liao</a></span>
     </div>
