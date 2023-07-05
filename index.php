53d66a2ef589ab17759435ffface12ec89f60cbd
Kent Liao <2964556627@qq.com>
2023-07-05 21:02:29 +0800

    style: adjust code style for better readability of the code

--- index.php
+++ index.php
@@ -17,19 +17,19 @@ if (!defined("__TYPECHO_ROOT_DIR__")) {
 <?php $this->need("header.php"); ?>
 <body class="jasmine-body">
 <div class="jasmine-container grid grid-cols-12">
-        <?php $this->need("component/sidebar-left.php"); ?>
-        <div class="flex col-span-12 lg:col-span-8 flex-col lg:border-x-2 border-stone-100 dark:border-neutral-600 lg:pt-0 lg:px-6 pb-10 px-3">
-            <?php $this->need("component/menu.php"); ?>
-            <?php if ($this->is("index") && $this->_currentPage == 1): ?>
-                <?php $this->need("component/post-top.php"); ?>
-            <?php endif; ?>
-            <?php $this->need("component/post-item.php"); ?>
-            <?php $this->need("component/paging.php"); ?>
-        </div>
-        <div class="hidden lg:col-span-3 lg:block" id="sidebar-right">
-            <?php $this->need("component/sidebar.php"); ?>
-        </div>
+    <?php $this->need("component/sidebar-left.php"); ?>
+    <div class="flex col-span-12 lg:col-span-8 flex-col lg:border-x-2 border-stone-100 dark:border-neutral-600 lg:pt-0 lg:px-6 pb-10 px-3">
+        <?php $this->need("component/menu.php"); ?>
+        <?php if ($this->is("index") && $this->_currentPage == 1): ?>
+            <?php $this->need("component/post-top.php"); ?>
+        <?php endif; ?>
+        <?php $this->need("component/post-item.php"); ?>
+        <?php $this->need("component/paging.php"); ?>
     </div>
+    <div class="hidden lg:col-span-3 lg:block" id="sidebar-right">
+        <?php $this->need("component/sidebar.php"); ?>
+    </div>
+</div>
     <?php $this->need("footer.php"); ?>
 </body>
 
