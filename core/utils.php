57f6fe535ec2da878f2e4bafcfcee5112ce8d095
Milk <1871357815@qq.com>
2023-08-19 20:28:47 +0800

    Move func getPostviews to utils.php

--- core/utils.php
+++ core/utils.php
@@ -9,6 +9,34 @@ if (!defined("__TYPECHO_ROOT_DIR__")) {
 }
 
 /**
+ * 获取阅读量
+ * @param $archive
+ * @return void
+ */
+function getPostviews($archive) {
+    $db = Typecho_Db::get();
+    $cid = $archive->cid;
+    if (!array_key_exists('views', $db->fetchRow($db->select()->from('table.contents')))) {
+        $db->query('ALTER TABLE `'.$db->getPrefix().'contents` ADD `views` INT(10) DEFAULT 0;');
+    }
+    $exist = $db->fetchRow($db->select('views')->from('table.contents')->where('cid = ?', $cid))['views'];
+    if ($archive->is('single')) {
+        $cookie = Typecho_Cookie::get('contents_views');
+        $cookie = $cookie ? explode(',', $cookie) : array();
+        if (!in_array($cid, $cookie)) {
+            $db->query($db->update('table.contents')
+                ->rows(array('views' => (int)$exist+1))
+                ->where('cid = ?', $cid));
+            $exist = (int)$exist+1;
+            array_push($cookie, $cid);
+            $cookie = implode(',', $cookie);
+            Typecho_Cookie::set('contents_views', $cookie);
+        }
+    }
+    echo $exist == 0 ? '   暂无阅读' :'   阅读量：' .$exist;
+}
+
+/**
  * 获取 Options
  * @return Options
  */
--- functions.php
+++ functions.php
@@ -6,29 +6,6 @@ error_reporting(E_ERROR);
 
 require_once "core/index.php";
 
-function getPostviews($archive) {
-    $db = Typecho_Db::get();
-    $cid = $archive->cid;
-    if (!array_key_exists('views', $db->fetchRow($db->select()->from('table.contents')))) {
-        $db->query('ALTER TABLE `'.$db->getPrefix().'contents` ADD `views` INT(10) DEFAULT 0;');
-    }
-    $exist = $db->fetchRow($db->select('views')->from('table.contents')->where('cid = ?', $cid))['views'];
-    if ($archive->is('single')) {
-        $cookie = Typecho_Cookie::get('contents_views');
-        $cookie = $cookie ? explode(',', $cookie) : array();
-        if (!in_array($cid, $cookie)) {
-            $db->query($db->update('table.contents')
-                ->rows(array('views' => (int)$exist+1))
-                ->where('cid = ?', $cid));
-            $exist = (int)$exist+1;
-            array_push($cookie, $cid);
-            $cookie = implode(',', $cookie);
-            Typecho_Cookie::set('contents_views', $cookie);
-        }
-    }
-    echo $exist == 0 ? '   暂无阅读' :'   阅读量：' .$exist;
-}
-
 /**
  * 初始化主题
  * @param $archive
