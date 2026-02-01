1afdea2d1a50ab3651c1816dbed24c4bb5206d3e
Kent Liao <2964556627@qq.com>
2026-02-01 11:03:56 +0800

    Compatible with new version

--- theme/template-parts/category-sub.php
+++ theme/template-parts/category-sub.php
@@ -1,9 +1,10 @@
-<?php use theme\widget\CategorySub;
+<?php
 
 if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
-<?php if(!empty($this->categories[0]['mid'])): ?>
-    <?php CategorySub::alloc(['parentId' => $this->categories[0]['mid']])->to($categoies) ?>
+<?php $parentId = (isset($this->pageRow) && is_array($this->pageRow) && !empty($this->pageRow['mid'])) ? $this->pageRow['mid'] : $this->categories[0]['mid']; ?>
+<?php if(!empty($parentId)): ?>
+    <?php Widget\Metas\Category\Rows::alloc()->to($categoies) ?>
     <?php if ($categoies->have()): ?>
         <div class="card border-0 py-1 col-12">
             <div class="d-flex flex-wrap row-gap-2 column-gap-3 sub-category">
@@ -11,9 +12,11 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
                     <?php $this->archiveTitle(); ?>
                 </a>
                 <?php while ($categoies->next()): ?>
-                    <a class="px-3 py-2 " href="<?php $categoies->permalink(); ?>" title="<?php $categoies->name(); ?>">
-                        <?php $categoies->name(); ?>
-                    </a>
+                    <?php if($categoies->parent == $parentId): ?>
+                        <a class="px-3 py-2 " href="<?php $categoies->permalink(); ?>" title="<?php $categoies->name(); ?>">
+                            <?php $categoies->name(); ?>
+                        </a>
+                    <?php endif; ?>
                 <?php endwhile; ?>
             </div>
         </div>
